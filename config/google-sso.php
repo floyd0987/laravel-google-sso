4d8a4889f748961710139e45020f4aeaef6c9339
Eronne Bernucci <eronne.bernucci@immedia.net>
2025-07-24 23:15:08 +0200

    GOOGLE_SSO_FRONTEND_URL

--- config/google-sso.php
+++ config/google-sso.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000/auth/callback'),
+    'frontend_url' => env('GOOGLE_SSO_FRONTEND_URL', 'http://localhost:3000/auth/callback'),
     'issue_token' => env('GOOGLE_SSO_ISSUE_TOKEN', true),
 ];
